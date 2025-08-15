4104349bb71ddf5bc7d5cb6a2e03b69a9d8c9a4b
Sebastian <ahss@aarhus.dk>
2025-08-15 12:45:10 +0200

    Remove redundant code for depreceated control table

--- robot_framework/subprocesses/helper_functions.py
+++ robot_framework/subprocesses/helper_functions.py
@@ -1,15 +1,9 @@
 """Module for helper functions"""
-import tempfile
 import pyodbc
-from office365.runtime.auth.user_credential import UserCredential
-from office365.sharepoint.client_context import ClientContext
-from office365.sharepoint.files.file import File
-import pandas as pd
-
-from robot_framework.config import SHAREPOINT_SITE_URL, DOCUMENT_LIBRARY
 
 
 def find_match_ovk(ovk: str):
+    """To find matching overenskomst, maybe?"""
     # Some lookup
     match_ovk = ""  
 
@@ -75,44 +69,3 @@ def get_items_from_query(connection_string, query: str):
         return None
 
     return result
-
-
-def fetch_files(username, password) -> dict:
-    """Download Excel files from SharePoint to the specified path."""
-
-    ctx = ClientContext(SHAREPOINT_SITE_URL).with_credentials(UserCredential(username, password))
-    target_folder_url = f"/teams/{SHAREPOINT_SITE_URL.rsplit('/teams/', maxsplit=1)[-1]}/{DOCUMENT_LIBRARY}"
-    target_folder = ctx.web.get_folder_by_server_relative_url(target_folder_url)
-    files = target_folder.files
-    ctx.load(files)
-    ctx.execute_query()
-
-    if not files:
-        print("No files found in the specified SharePoint folder.")
-
-    file_table = None
-
-    for file in files:
-        if file.name == 'Styretabel.xlsx':
-            file_content = File.open_binary(ctx, file.serverRelativeUrl)
-            with tempfile.NamedTemporaryFile(mode='wb', suffix=".xlsx", delete=True, delete_on_close=False) as temp_file:
-                # Write to temp
-                temp_file.write(file_content.content)
-                temp_file_path = temp_file.name
-
-                # Read into pandas dataframe
-                file_table = pd.read_excel(temp_file_path, skiprows=1, usecols=[1, 2, 3, 4])
-
-        else:
-            pass
-
-    control_table = {
-        row[1].iloc[0]: {
-            'worker_type': row[1].iloc[2],
-            'worker_data': row[1].iloc[3],
-            'process_description': row[1].iloc[1]
-        }
-        for row in file_table.where(pd.notnull(file_table), None).iterrows()
-    }
-
-    return control_table
